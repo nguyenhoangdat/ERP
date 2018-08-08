b2303956f1072e6b56df0cdef18d95ad296f593c
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2018-08-08 09:31:01 +0200

    Warehouse.API: Controllers added

---
+++ src/Services/Warehouse/Warehouse.API/Controllers/StockTakingItemsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.API.Models;
+using Warehouse.API.Models.StockTaking;
+
+namespace Warehouse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockTakingItemsController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public StockTakingItemsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StockTakingItems
+        [HttpGet]
+        public IEnumerable<StockTakingItem> GetGetStockTakingItems()
+        {
+            return _context.GetStockTakingItems;
+        }
+
+        // GET: api/StockTakingItems/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStockTakingItem([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockTakingItem = await _context.GetStockTakingItems.FindAsync(id);
+
+            if (stockTakingItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stockTakingItem);
+        }
+
+        // PUT: api/StockTakingItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStockTakingItem([FromRoute] long id, [FromBody] StockTakingItem stockTakingItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != stockTakingItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stockTakingItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockTakingItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/StockTakingItems
+        [HttpPost]
+        public async Task<IActionResult> PostStockTakingItem([FromBody] StockTakingItem stockTakingItem)
+        {
+            this.ModelState.Remove("Id");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.GetStockTakingItems.Add(stockTakingItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStockTakingItem", new { id = stockTakingItem.Id }, stockTakingItem);
+        }
+
+        // DELETE: api/StockTakingItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStockTakingItem([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockTakingItem = await _context.GetStockTakingItems.FindAsync(id);
+            if (stockTakingItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.GetStockTakingItems.Remove(stockTakingItem);
+            await _context.SaveChangesAsync();
+
+            return Ok(stockTakingItem);
+        }
+
+        private bool StockTakingItemExists(long id)
+        {
+            return _context.GetStockTakingItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/Services/Warehouse/Warehouse.API/Controllers/StockTakingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Warehouse.API.Models;
+using Warehouse.API.Models.StockTaking;
+
+namespace Warehouse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockTakingsController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public StockTakingsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/StockTakings
+        [HttpGet]
+        public IEnumerable<StockTaking> GetStockTakings()
+        {
+            return _context.StockTakings;
+        }
+
+        // GET: api/StockTakings/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStockTaking([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockTaking = await _context.StockTakings.FindAsync(id);
+
+            if (stockTaking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stockTaking);
+        }
+
+        // PUT: api/StockTakings/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStockTaking([FromRoute] int id, [FromBody] StockTaking stockTaking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != stockTaking.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(stockTaking).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StockTakingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/StockTakings
+        [HttpPost]
+        public async Task<IActionResult> PostStockTaking([FromBody] StockTaking stockTaking)
+        {
+            this.ModelState.Remove("Id");
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.StockTakings.Add(stockTaking);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStockTaking", new { id = stockTaking.Id }, stockTaking);
+        }
+
+        // DELETE: api/StockTakings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStockTaking([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var stockTaking = await _context.StockTakings.FindAsync(id);
+            if (stockTaking == null)
+            {
+                return NotFound();
+            }
+
+            _context.StockTakings.Remove(stockTaking);
+            await _context.SaveChangesAsync();
+
+            return Ok(stockTaking);
+        }
+
+        private bool StockTakingExists(int id)
+        {
+            return _context.StockTakings.Any(e => e.Id == id);
+        }
+    }
+}
