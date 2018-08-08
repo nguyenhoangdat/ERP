0895f50336ad0c14ccfcf119e0554ffbadd17fed
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2018-08-08 20:04:45 +0200

    Warehouse.API.Models.Extensions: Refactoring

--- src/Services/Warehouse/Warehouse.API.Models.Extensions/PositionExtension.cs
+++ src/Services/Warehouse/Warehouse.API.Models.Extensions/PositionExtension.cs
@@ -5,34 +5,16 @@ namespace Warehouse.API.Models.Extensions
 {
     public static class PositionExtension
     {
-        public static int? Count(this Position position)
+        public static int Count(this Position position)
         {
             if (position == null)
             {
                 throw new ArgumentNullException(nameof(position));
             }
 
-            return position.Movements.OrderByDescending(x => x.MovementDate).FirstOrDefault()?.CountTotal;
-        }
+            int? count = position.Movements.OrderByDescending(x => x.MovementDate).FirstOrDefault()?.CountTotal;
 
-        public static StoredItem StoredItem(this Position position)
-        {
-            if (position == null)
-            {
-                throw new ArgumentNullException(nameof(position));
-            }
-
-            return position.Movements.OrderByDescending(x => x.MovementDate).FirstOrDefault()?.StoredItem;
-        }
-
-        public static int? StoredItemId(this Position position)
-        {
-            if (position == null)
-            {
-                throw new ArgumentNullException(nameof(position));
-            }
-
-            return position.Movements.OrderByDescending(x => x.MovementDate).FirstOrDefault()?.StoredItemId;
+            return count ?? 0;
         }
     }
 }
--- src/Services/Warehouse/Warehouse.API.Models.Extensions/StoredItemExtension.cs
+++ src/Services/Warehouse/Warehouse.API.Models.Extensions/StoredItemExtension.cs
@@ -1,27 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace Warehouse.API.Models.Extensions
 {
     public static class StoredItemExtension
     {
-        public static int? Count(this StoredItem storedItem)
+        public static int Count(this StoredItem storedItem)
         {
             if (storedItem == null)
             {
                 throw new ArgumentNullException(nameof(storedItem));
             }
 
-            int count = 0;
-
-            foreach (Position item in storedItem.Positions)
-            {
-                int? itemCount = item.Count();
-
-                if (itemCount.HasValue)
-                    count += itemCount.Value;
-            }
-
-            return count;
+            return storedItem.Positions.Sum(x => x.Count());
         }
     }
 }
