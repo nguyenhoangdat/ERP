472de7491ff2219b50b99d952d77957238e4fe82
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2018-09-02 22:22:18 +0200

    Solution: Added project Catalog.API

---
+++ src/Services/Catalog/Catalog.API.Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Models
+{
+    public class Category
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API.Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+
+        [Required, Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Required, Url]
+        public string URLAddress { get; set; }
+    }
+}
