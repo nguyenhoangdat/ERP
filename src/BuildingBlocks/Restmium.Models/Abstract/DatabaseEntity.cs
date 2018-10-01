e412eca0a63f213aefcec8033a6d88dc67a6fc79
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2018-10-01 09:58:56 +0100

    Restmium.Models: DatabaseEntity properties renamed

--- src/BuildingBlocks/Restmium.Models/Abstract/DatabaseEntity.cs
+++ src/BuildingBlocks/Restmium.Models/Abstract/DatabaseEntity.cs
@@ -6,7 +6,7 @@ namespace Restmium.Models.Abstract
     public abstract class DatabaseEntity
     {
         [Required]
-        public DateTime CreatedAt { get; set; }
-        public DateTime DeletedAt { get; set; }
+        public DateTime UtcCreatedAt { get; set; }
+        public DateTime UtcDeletedAt { get; set; }
     }
 }
