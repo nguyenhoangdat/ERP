bbbbdc02b5ce8debf3134495c8d7f5f37116da00
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-08-09 23:04:35 +0200

    Update UpdateIssueSlipItemRequestedUnitsCommand.cs

--- src/Services/Warehouse/Warehouse.Application/Commands/UpdateIssueSlipItemRequestedUnitsCommand.cs
+++ src/Services/Warehouse/Warehouse.Application/Commands/UpdateIssueSlipItemRequestedUnitsCommand.cs
@@ -5,14 +5,16 @@ namespace Restmium.ERP.Services.Warehouse.Application.Commands
 {
     public class UpdateIssueSlipItemRequestedUnitsCommand : IRequest<IssueSlip.Item>
     {
-        public UpdateIssueSlipItemRequestedUnitsCommand(long issueSlipId, int wareId, int requestedUnits)
+        public UpdateIssueSlipItemRequestedUnitsCommand(long issueSlipId, long? positionId, int wareId, int requestedUnits)
         {
             this.IssueSlipId = issueSlipId;
+            this.PositionId = positionId;
             this.WareId = wareId;
             this.RequestedUnits = requestedUnits;
         }
 
         public long IssueSlipId { get; }
+        public long? PositionId { get; }
         public int WareId { get; }
         public int RequestedUnits { get; }
     }
