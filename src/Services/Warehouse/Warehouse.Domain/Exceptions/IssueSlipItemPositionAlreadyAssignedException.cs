5efbebe4cf6f7e3142f84c3734a57aa899a33f24
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-08-09 21:36:01 +0200

    Services.Warehouse: Add new Exceptions needed to issue ware

---
+++ src/Services/Warehouse/Warehouse.Domain/Exceptions/IssueSlipItemPositionAlreadyAssignedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Restmium.ERP.Services.Warehouse.Domain.Exceptions
+{
+    public class IssueSlipItemPositionAlreadyAssignedException : Exception
+    {
+        public IssueSlipItemPositionAlreadyAssignedException()
+        {
+        }
+
+        public IssueSlipItemPositionAlreadyAssignedException(string message) : base(message)
+        {
+        }
+
+        public IssueSlipItemPositionAlreadyAssignedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IssueSlipItemPositionAlreadyAssignedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Services/Warehouse/Warehouse.Domain/Exceptions/IssueSlipItemPositionAvailableUnitsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Restmium.ERP.Services.Warehouse.Domain.Exceptions
+{
+    public class IssueSlipItemPositionAvailableUnitsException : Exception
+    {
+        public IssueSlipItemPositionAvailableUnitsException()
+        {
+        }
+
+        public IssueSlipItemPositionAvailableUnitsException(string message) : base(message)
+        {
+        }
+
+        public IssueSlipItemPositionAvailableUnitsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected IssueSlipItemPositionAvailableUnitsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
