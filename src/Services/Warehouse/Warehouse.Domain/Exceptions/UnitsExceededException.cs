d0981bb10519c4031efb62321ca595523eef55e1
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-08-21 12:06:31 +0200

    Services.Warehouse: Renamed IssueSlipItemRequestedUnitsExceededException

---
+++ src/Services/Warehouse/Warehouse.Domain/Exceptions/UnitsExceededException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Restmium.ERP.Services.Warehouse.Domain.Exceptions
+{
+    public class UnitsExceededException : Exception
+    {
+        public UnitsExceededException()
+        {
+        }
+
+        public UnitsExceededException(string message) : base(message)
+        {
+        }
+
+        public UnitsExceededException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnitsExceededException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
