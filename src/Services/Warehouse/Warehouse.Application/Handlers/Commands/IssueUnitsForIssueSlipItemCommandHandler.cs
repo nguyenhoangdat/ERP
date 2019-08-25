db22290a65ce22ced3616f876055835520facc9a
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-08-25 19:53:50 +0200

    Services.Warehouse: Updated IssueUnitsForIssueSlipItemCommandHandler

--- src/Services/Warehouse/Warehouse.Application/Handlers/Commands/IssueUnitsForIssueSlipItemCommandHandler.cs
+++ src/Services/Warehouse/Warehouse.Application/Handlers/Commands/IssueUnitsForIssueSlipItemCommandHandler.cs
@@ -2,9 +2,9 @@
 using Restmium.ERP.Services.Warehouse.Application.Commands;
 using Restmium.ERP.Services.Warehouse.Domain.Entities;
 using Restmium.ERP.Services.Warehouse.Domain.Entities.Extensions;
+using Restmium.ERP.Services.Warehouse.Domain.Events;
 using Restmium.ERP.Services.Warehouse.Domain.Exceptions;
 using Restmium.ERP.Services.Warehouse.Infrastructure.Database;
-using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -93,6 +93,8 @@ namespace Restmium.ERP.Services.Warehouse.Application.Handlers.Commands
 
                 await this.Mediator.Send(new CreateMovementCommand(item.WareId, item.PositionId.Value, Movement.Direction.Out, request.Count), cancellationToken);
             }
+
+            await this.Mediator.Publish(new IssueSlipItemUpdatedDomainEvent(item), cancellationToken);
 
             return item;
         }
