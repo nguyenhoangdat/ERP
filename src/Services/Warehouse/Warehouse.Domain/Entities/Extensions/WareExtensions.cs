264d9982776d81abf40d46432bf2e311eaa21aba
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-08-28 14:02:01 +0200

    Services.Warehouse: Created WareExtensions.cs

---
+++ src/Services/Warehouse/Warehouse.Domain/Entities/Extensions/WareExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Restmium.ERP.Services.Warehouse.Domain.Entities.Extensions
+{
+    public static class WareExtensions
+    {
+        public static bool CanBeMovedToBin(this Ware ware)
+        {
+            if (ware.UtcMovedToBin != null)
+            {
+                return false;
+            }
+
+            foreach (StockTaking.Item item in ware.StockTakingItems.Where(x => x.UtcMovedToBin == null))
+            {
+                if (item.CanBeMovedToBin() == false)
+                {
+                    return false;
+                }
+            }
+            foreach (Receipt.Item item in ware.ReceiptItems.Where(x => x.UtcMovedToBin == null))
+            {
+                if (item.CanBeMovedToBin() == false)
+                {
+                    return false;
+                }
+            }
+            foreach (StockTaking.Item item in ware.StockTakingItems.Where(x => x.UtcMovedToBin == null))
+            {
+                if (item.CanBeMovedToBin() == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
