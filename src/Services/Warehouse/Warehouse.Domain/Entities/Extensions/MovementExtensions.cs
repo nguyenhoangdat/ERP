651b1390fc1b69dbdbb0a1382ae037cfcb0b2d30
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2019-09-24 17:34:20 +0100

    Warehouse.Domain: Updated Movement.CanBeMovedToBin()

--- src/Services/Warehouse/Warehouse.Domain/Entities/Extensions/MovementExtensions.cs
+++ src/Services/Warehouse/Warehouse.Domain/Entities/Extensions/MovementExtensions.cs
@@ -1,18 +1,24 @@
-﻿namespace Restmium.ERP.Services.Warehouse.Domain.Entities.Extensions
+﻿using System;
+using System.Linq;
+
+namespace Restmium.ERP.Services.Warehouse.Domain.Entities.Extensions
 {
     public static class MovementExtensions
     {
         public static bool CanBeMovedToBin(this Movement movement)
         {
-            return movement.UtcMovedToBin == null;
+            return
+                movement.UtcMovedToBin == null && // Not in a bin
+                movement.UtcDelete <= DateTime.UtcNow && // Retention period has passed
+                movement.Position.Movements.Any(x => x.UtcCreated > movement.UtcCreated); // Newer movement exists
         }
 
         public static bool CanBeRestoredFromBin(this Movement movement)
         {
             return
                 movement.UtcMovedToBin != null &&
-                movement.Ware.CanBeMovedToBin() &&
-                movement.Position.CanBeMovedToBin();
+                movement.Ware.UtcMovedToBin == null &&
+                movement.Position.UtcMovedToBin == null;
         }
     }
 }
